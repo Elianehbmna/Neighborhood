0beb5d2dc3dd882f34360cf77e242452b726a147
Elianehbmna <elianehabimana3@gmail.com>
2019-10-31 15:06:09 +0200

    create the profile model

--- neighbor/models.py
+++ neighbor/models.py
@@ -6,3 +6,12 @@ import datetime as dt
 from tinymce.models import HTMLField
 
 
+class Profile(models.Model):
+    bio=models.TextField(max_length=100,blank=True,default="bio please...")
+    profilepic=models.ImageField(upload_to='profile/', blank = True,default='../static/images/bad-profile-pic-2.jpeg')
+    email=models.CharField(blank=True,max_length=100)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user
